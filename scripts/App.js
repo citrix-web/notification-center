d287b85481f6ece26992ce36847f3db29fc4e5c9
Ninad Thepade <ninad.thepade@citrix.com>
2016-04-20 10:49:04 -0700

    Adding messageList component

--- scripts/App.js
+++ scripts/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import SassPlayground from './SassPlayground';
+import MessageList from './MessageList';
 
 export default class App extends Component {
     render() {
