a599c92785ff33aaf3e071a5c5011c9fedaec58c
Mike Leong <mleong28@gmail.com>
2016-04-21 12:45:39 -0700

    socket

--- scripts/App.js
+++ scripts/App.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import NotificationCenter from './NotificationCenter';
 
 export default class App extends Component {
+  componentDidMount() {
+    var socket = io.connect('http://localhost:3000');
+    socket.on('connect', function(data) {
+      socket.on('messages', function(data) {
+        console.log(data);
+      });
+    });
+  }
+
   render() {
     return (
       <div>
